6e56439bc847861c56186dd4117413cf4d1ab034
GitHub Action <action@github.com>
2023-05-18 07:32:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/hkhhh/exposedgenerator/MyBundle.kt
+++ src/main/kotlin/com/github/hkhhh/exposedgenerator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hkhhh.exposedgenerator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hkhhh/exposedgenerator/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/hkhhh/exposedgenerator/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hkhhh.exposedgenerator.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
